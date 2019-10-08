eb45de72294bc1bfab32eb3440c410af64de676b
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-08 23:59:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,6 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value. each do |new_value, names|
       names.each do |name|
-        if !data
+        if !pigeon_list[name]
+          pigeon_list
 end
