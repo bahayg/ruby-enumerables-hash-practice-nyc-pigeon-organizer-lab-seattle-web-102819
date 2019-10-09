96bd20c568b3c42847c58176c937a7fb086f7c74
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-10-09 00:05:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
           pigeon_list[name][key] = []
         end
         
-        pigeon_list[name][key].push(new_value).to_s
+        pigeon_list[name][key].push(new_value.to_s)
       end
     end
   end
